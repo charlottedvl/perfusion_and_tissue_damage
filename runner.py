6dec5f8c7bf1e841191330959bfd86ebc8004164
Max van der Kolk <m.vanderkolk@uva.nl>
2020-09-15 18:03:35 +0200

    Update Dockerfile to `fenics`-based container.
    This allows to start from an image where fenics is already build. The
    download time is longer, while the number packages to compile is
    reduced. The Python requirements are move into `requirements.txt`.
    Extracting the meshes is done by the `ADD` command.
    
    Note, this version does _not_ contain MPI support yet.

--- runner.py
+++ runner.py
@@ -1,9 +1,11 @@
+#!/usr/env/python3
+
 import os
 os.chdir('./perfusion')
 os.system('ls -lha')
 os.system('pwd')
 if not os.path.exists('../brain_meshes/b0000/permeability/K1_form.xdmf'):
-    os.system('mpirun --allow-run-as-root --mca btl_vader_single_copy_mechanism none -n 6 python3 permeability_initialiser.py')
-    
+    os.system('python3 permeability_initialiser.py')
+
 os.system('python3 BC_creator.py')
-os.system('mpirun --allow-run-as-root --mca btl_vader_single_copy_mechanism none -n 6 python3 basic_flow_solver.py')
+os.system('python3 basic_flow_solver.py')

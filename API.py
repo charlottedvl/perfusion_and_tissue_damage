c81c3e98f04ece1095fe84fe5eb2ff22f9045784
Max van der Kolk <m.vanderkolk@uva.nl>
2020-10-21 14:55:33 +0200

    Read boundary conditions from blood-flow.
    Ensures the boundary conditions for the perfusion model are read from
    the output created by the one-dimensional blood-flow analysis.
    Additionally, the output is directed to separated paths for perfusion.

--- API.py
+++ API.py
@@ -3,31 +3,57 @@ import os
 import subprocess
 import sys
 
+# Default path (inside the container) pointing to the YAML configuration for
+# the `basic_flow_solver` routine.
+perfusion_config_file = '/app/perfusion/config_basic_flow_solver.yml'
+blood_flow_dir = 'bf_sim'
+perfusion_dir = 'pf_sim'
+
 
 class API(eventhandler.EventHandler):
     def handle_event(self):
         perm_file = '/app/brain_meshes/b0000/permeability/K1_form.xdmf'
+
         if not os.path.exists(perm_file):
-            subprocess.run(["python3", "permeability_initialiser.py"],
-                           check=True,
-                           cwd="./perfusion")
+            permeability_cmd = ["python3", "permeability_initialiser.py"]
 
-        # output paths for `BC_creator.py` and `basic_flow_solver.py`
-        bc_folder = self.patient_dir.joinpath("BC_result")
-        res_folder = self.patient_dir.joinpath("VP_result")
+            print(
+                f"Permeability file '{perm_file}' not present..."
+                f"Evaluating: '{' '.join(permeability_cmd)}'",
+                flush=True)
 
-        subprocess.run([
-            "python3", "BC_creator.py", "--folder", f"{bc_folder}/",
-            "--res_fldr", f"{res_folder}/"
-        ],
-                       check=True,
-                       cwd="./perfusion")
+            subprocess.run(permeability_cmd, check=True, cwd="/app/perfusion")
 
-        subprocess.run([
-            "python3", "basic_flow_solver.py", "--res_fldr", f"{res_folder}/"
-        ],
-                       check=True,
-                       cwd="./perfusion")
+        # output paths for perfusion simulation
+        res_folder = self.result_dir.joinpath(f"{perfusion_dir}/VP_result")
+        os.makedirs(res_folder, exist_ok=True)
+
+        # update configuration for perfusion
+        solver_config = eventhandler.read_yaml(perfusion_config_file)
+
+        # ensure boundary conditions are being read from input files
+        solver_config['input']['read_inlet_boundary'] = True
+        bc_file = self.result_dir.joinpath(
+            f'{blood_flow_dir}/boundary_condition_file.csv')
+        solver_config['input']['inlet_boundary_file'] = str(bc_file.resolve())
+
+        # cannot proceed without boundary conditions
+        msg = f"Boundary conditions `1d-blood-flow` not present: `{bc_file}`"
+        assert os.path.isfile(bc_file), msg
+
+        # update output settings
+        config_path = self.result_dir.joinpath(
+            f'{perfusion_dir}/perfusion_config.yml')
+        eventhandler.write_yaml(solver_config, config_path)
+
+        # form command to evaluate perfusion
+        solve_cmd = [
+            "python3", "basic_flow_solver.py", "--res_fldr", f"{res_folder}/",
+            "--config_file", f"{str(config_path)}"
+        ]
+
+        print(f"Evaluating: '{' '.join(solve_cmd)}'", flush=True)
+        subprocess.run(solve_cmd, check=True, cwd="/app/perfusion")
 
     def handle_example(self):
         self.handle_event()

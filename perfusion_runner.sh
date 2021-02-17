1dbc131e9eb24aa2ba202db010db958738197a1f
Tamas Jozsa <tamas.jozsa@eng.ox.ac.uk>
2021-02-17 19:34:36 +0000

    streamline RMCA occlusion simulation

--- perfusion_runner.sh
+++ perfusion_runner.sh
@@ -8,5 +8,5 @@ fi
 
 cd perfusion
 mpirun -n 6 python3 permeability_initialiser.py
-python3 BC_creator.py
 mpirun -n 6 python3 basic_flow_solver.py
+mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_RMCAo.yaml

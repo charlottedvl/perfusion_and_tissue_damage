7dcc27612c6a333b24ff85bbd2f88b561b16324b
Tamas Jozsa <tamas.jozsa@eng.ox.ac.uk>
2021-02-24 09:00:07 +0000

    RMCA/LMCA occl. files added

--- perfusion_runner.sh
+++ perfusion_runner.sh
@@ -9,4 +9,7 @@ fi
 cd perfusion
 mpirun -n 6 python3 permeability_initialiser.py
 mpirun -n 6 python3 basic_flow_solver.py
+mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_LMCAo.yaml
 mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_RMCAo.yaml
+mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_LMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_LMCAo/perfusion.xdmf
+mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_RMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_RMCAo/perfusion.xdmf

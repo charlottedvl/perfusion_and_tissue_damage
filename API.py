917311e25836893a3a0d13c4e76c2a0e5b8f46c3
Max van der Kolk <m.vanderkolk@uva.nl>
2021-05-24 11:15:20 +0200

    Update to new API event handler.

--- API.py
+++ API.py
@@ -1,16 +1,44 @@
-from perfusion.API import API as perfusion_API
-from oxygen.API import API as oxygen_API
+from perfusion.API import API as Perfusion_API
+from oxygen.API import API as Oxygen_API
 import sys
 
 
+from desist.eventhandler.api import API
+from desist.eventhandler.eventhandler import event_handler
+
+
+class Dispatch(API):
+    """A wrapper API that dispatches to the right simulation.
+
+    To choose the right part of the perfusion simulation, we dispatch on the
+    type of the current event. The ``select_api`` routine returns the
+    corresponding API implementation that can be evaluated.
+    """
+    def select_api(self):
+        if self.current_model.get('type') == 'PERFUSION':
+            api = event_handler(Perfusion_API)
+            return api
+
+        if self.current_model.get('type') == 'OXYGEN':
+            api = event_handler(Oxygen_API)
+            return api
+
+        print(f"No API has been evaluated for model: `{self.current_model}`.")
+        sys.exit(1)
+
+    def event(self):
+        api = self.select_api()
+        return api()
+
+    def example(self):
+        api = self.select_api()
+        return api()
+
+    def test(self):
+        api = self.select_api()
+        return api()
+
+
 if __name__ == "__main__":
-    api = perfusion_API(sys.argv[1:])
-    analysis = api.model['type']
-
-    if analysis == 'PERFUSION':
-        sys.exit(perfusion_API(sys.argv[1:]).evaluate())
-
-    if analysis == 'OXYGEN':
-        sys.exit(oxygen_API(sys.argv[1:]).evaluate())
-
-    sys.exit("No model was evaluated.")
+    api = event_handler(Dispatch)
+    api()
--- oxygen/API.py
+++ oxygen/API.py
@@ -1,5 +1,7 @@
-from eventmodule import eventhandler
 import subprocess
+
+from desist.eventhandler.api import API
+from desist.isct.utilities import read_yaml, write_yaml
 
 # Default path (inside container) for configuration files
 oxygen_config_file = '/app/oxygen/config_oxygen_solver.yaml'
@@ -7,20 +9,20 @@ perfusion_config_file = '/app/perfusion/config_basic_flow_solver.yaml'
 perfusion_dir = 'pf_sim'
 
 
-class API(eventhandler.EventHandler):
-    def handle_event(self):
+class API(API):
+    def event(self):
 
         # output paths
         res_folder = self.result_dir.joinpath(f"{perfusion_dir}")
 
         # read properties from previous perfusion step
-        perfusion_config = eventhandler.read_yaml(
+        perfusion_config = read_yaml(
             f'{res_folder}/perfusion_config.yaml')
         bc_file = perfusion_config.get('input', {}).get('inlet_boundary_file')
         assert bc_file is not None, "no path for inlet boundary conditions"
 
         # read configuration for oxygen
-        solver_config = eventhandler.read_yaml(oxygen_config_file)
+        solver_config = read_yaml(oxygen_config_file)
 
         # update configuration
         solver_config['input']['para_path'] = f'{res_folder.resolve()}/'
@@ -30,7 +32,7 @@ class API(eventhandler.EventHandler):
         # write configuration to disk
         config_path = self.result_dir.joinpath(
             f'{perfusion_dir}/oxygen_config.yaml')
-        eventhandler.write_yaml(solver_config, config_path)
+        write_yaml(config_path, solver_config)
 
         # setup command for oxygen model with arguments
         oxygen_cmd = [
@@ -42,8 +44,8 @@ class API(eventhandler.EventHandler):
         print(f"Evaluting: '{' '.join(oxygen_cmd)}'", flush=True)
         subprocess.run(oxygen_cmd, check=True, cwd="/app/oxygen")
 
-    def handle_example(self):
-        self.handle_event()
+    def example(self):
+        self.event()
 
-    def handle_test(self):
-        self.handle_example()
+    def test(self):
+        self.example()

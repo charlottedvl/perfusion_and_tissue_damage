370c35f418c64de708da44b6b9a90b7f931d5a1a
Max van der Kolk <m.vanderkolk@uva.nl>
2021-08-31 11:07:06 +0200

    wip: API to support coupled and one-way operation.
    Also splits out the initialisation functionality in separate functions.

--- perfusion/API.py
+++ perfusion/API.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import subprocess
 
 from desist.eventhandler.api import API
@@ -6,8 +7,8 @@ from desist.isct.utilities import read_yaml, write_yaml
 
 # Default path (inside the container) pointing to the YAML configuration for
 # the `basic_flow_solver` routine.
-permeability_config_file = '/app/perfusion/config_permeability_initialiser.yaml'
-perfusion_config_file = '/app/perfusion/config_basic_flow_solver.yaml'
+permeability_config_file = 'config_permeability_initialiser.yaml'
+perfusion_config_file = 'config_basic_flow_solver.yaml'
 blood_flow_dir = 'bf_sim'
 perfusion_dir = 'pf_sim'
 # filename used for boundary conditions in CSV format
@@ -18,86 +19,126 @@ pf_outfile = 'perfusion.xdmf'
 brain_mesh_dir = 'brain_meshes'
 
 
+def initialise_brain_mesh(patient_dir, out_dir, cwd):
+    brain_mesh = patient_dir.joinpath(brain_mesh_dir)
+    clustered_mesh = out_dir.joinpath(f'{blood_flow_dir}/clustered_mesh.msh')
+
+    if brain_mesh.exists():
+        return 0
+
+    brain_mesh_cmd = [
+            "python3",
+            "convert_msh2hdf5.py",
+            str(clustered_mesh),
+            str(brain_mesh.joinpath('clustered')),
+    ]
+
+    print(f"Evaluating: '{' '.join(brain_mesh_cmd)}'", flush=True)
+    subprocess.run(brain_mesh_cmd, check=True, cwd=str(cwd))
+
+    error_msg = f"""The brain mesh or clustered files are missing on the
+    file system although the initialisation has been evaluated. This is not
+    supported. Please investigate: {brain_mesh} and {clustered_mesh}
+    paths."""
+    assert brain_mesh.exists() and clustered_mesh.exists(), error_msg
+
+
+def initialise_permeability(patient_dir, result_dir, cwd):
+    brain_mesh = patient_dir.joinpath(brain_mesh_dir)
+    permeability_dir = brain_mesh.joinpath('permeability')
+
+    if permeability_dir.exists():
+        return 0
+
+    clustered_xdmf = brain_mesh.joinpath('clustered.xdmf')
+    config_path = result_dir.joinpath(f'{perfusion_dir}/perm_config.yaml')
+
+    config = read_yaml(f'{cwd}/{permeability_config_file}')
+    config['input']['mesh_file'] = str(clustered_xdmf)
+    config['output']['res_fldr'] = f'{permeability_dir}/'
+    write_yaml(config_path, config)
+
+    permeability_cmd = [
+        "python3",
+        "permeability_initialiser.py",
+        "--config_file",
+        str(config_path)
+    ]
+
+    print(f"Evaluating: '{' '.join(permeability_cmd)}'", flush=True)
+    subprocess.run(permeability_cmd, check=True, cwd=str(cwd))
+
+    error_msg = f"""The permeability files do not exist, which should have
+    been generated at '{permeability_dir}' using
+    'permeability_initialiser.py' and its configuration at
+    '{config_path}'."""
+    assert permeability_dir.exists(), error_msg
+
+
 class API(API):
+    def __init__(self, patient, model_id, coupled=False, **kwargs):
+        super().__init__(patient, model_id)
+        self.coupled = coupled
+
     def event(self):
+        if self.coupled:
+            cwd = pathlib.Path('/app/perfusion_and_tissue_damage/perfusion')
+            solver = "coupled"
+        else:
+            cwd = pathlib.Path('/app/perfusion')
+            solver = "basic"
 
-        # patient's brain meshes and permeability information
-        brain_meshes = self.patient_dir.joinpath(brain_mesh_dir)
-        permeability_dir = brain_meshes.joinpath('permeability')
-
-        if not brain_meshes.exists():
-            error_msg = f"""Brain meshes and permeability files are not present
-            although previous events have been evaluated. This is not supported
-            and requires investigation why {brain_meshes} or {permeability_dir}
-            are not present anymore on the system."""
-            assert self.event_id == 0, error_msg
-
-            # generate clustering files from blood flow output
-            clustered_mesh = self.result_dir.joinpath(f'{blood_flow_dir}/clustered_mesh.msh')
-            clustering_result_dir = brain_meshes.joinpath('clustered')
-
-            brain_mesh_cmd = [
-                    "python3",
-                    "convert_msh2hdf5.py",
-                    str(clustered_mesh),
-                    str(clustering_result_dir)
-            ]
-            print(f"Evaluating: '{' '.join(brain_mesh_cmd)}'", flush=True)
-            subprocess.run(brain_mesh_cmd, check=True, cwd="/app/perfusion")
-
-        if not permeability_dir.exists():
-            # generate permeability meshes after clustering
-            clustered_mesh = brain_meshes.joinpath('clustered.xdmf')
-
-            perm_config = read_yaml(permeability_config_file)
-            perm_config['input']['mesh_file'] = str(clustered_mesh)
-            perm_config['output']['res_fldr'] = f'{permeability_dir}/'
-
-            config_path = self.result_dir.joinpath(f'{perfusion_dir}/perm_config.yaml')
-            write_yaml(config_path, perm_config)
-
-            permeability_cmd = [
-                "python3",
-                "permeability_initialiser.py",
-                "--config_file",
-                str(config_path)
-            ]
-            print(f"Evaluating: '{' '.join(permeability_cmd)}'", flush=True)
-            subprocess.run(permeability_cmd, check=True, cwd="/app/perfusion")
-
-        assert brain_meshes.exists(), f"Brain meshes not at: '{brain_meshes}'."
+        # ensure the clustered brain mesh is present
+        initialise_brain_mesh(self.patient_dir, self.result_dir, cwd)
+        initialise_permeability(self.patient_dir, self.result_dir, cwd)
 
         # output paths for perfusion simulation
         res_folder = self.result_dir.joinpath(f"{perfusion_dir}")
         os.makedirs(res_folder, exist_ok=True)
 
         # update configuration for perfusion
-        solver_config = read_yaml(perfusion_config_file)
+        perfusion_config_file = "config_{}_flow_solver.yaml".format(solver)
+        config = read_yaml(cwd.joinpath(perfusion_config_file))
 
         # ensure boundary conditions are being read from input files
-        solver_config['input']['read_inlet_boundary'] = True
+        config['input']['read_inlet_boundary'] = True
         bc_file = self.result_dir.joinpath(f'{blood_flow_dir}/{bc_fn}')
-        solver_config['input']['inlet_boundary_file'] = str(bc_file.resolve())
-        solver_config['input']['mesh_file'] = str(self.patient_dir.joinpath('brain_meshes/clustered.xdmf'))
-        solver_config['input']['permeability_folder'] = f"{self.patient_dir.joinpath('brain_meshes/permeability')}/"
+        config['input']['inlet_boundary_file'] = str(bc_file.resolve())
+        mesh_file = self.patient_dir.joinpath('brain_meshes/clustered.xdmf')
+        config['input']['mesh_file'] = str(mesh_file)
+        perm_file = self.patient_dir.joinpath('brain_meshes/permeability')
+        config['input']['permeability_folder'] = f"{perm_file}/"
+
+        # FIXME: this is only for testing purposes
+        config['simulation']['fe_degr'] = 1
 
         # cannot proceed without boundary conditions
-        msg = f"Boundary conditions `1d-blood-flow` not present: `{bc_file}`"
-        assert os.path.isfile(bc_file), msg
+        if not self.coupled:
+            msg = f"""Boundary conditions `1d-blood-flow` not present:
+                `{bc_file}`"""
+            assert os.path.isfile(bc_file), msg
 
         # update output settings
         config_path = self.result_dir.joinpath(
             f'{perfusion_dir}/perfusion_config.yaml')
-        write_yaml(config_path, solver_config)
+        write_yaml(config_path, config)
 
         # form command to evaluate perfusion
         solve_cmd = [
-            "python3", "basic_flow_solver.py", "--res_fldr", f"{res_folder}/",
+            "python3",
+            "{}_flow_solver.py".format(solver),
+            "--res_fldr", f"{res_folder}/",
             "--config_file", f"{str(config_path)}"
         ]
 
+        if self.coupled:
+            if self.event_id == 0:
+                solve_cmd.append('--healthy_scenario')
+            if self.patient_dir.joinpath('clot_present').exists():
+                solve_cmd.append('--clot-present')
+
         print(f"Evaluating: '{' '.join(solve_cmd)}'", flush=True)
-        subprocess.run(solve_cmd, check=True, cwd="/app/perfusion")
+        subprocess.run(solve_cmd, check=True, cwd=str(cwd))
 
         # terminate baseline scenario
         if self.event_id == 0:

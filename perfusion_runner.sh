f4c7190faca42b55473bc39d01141e85496d3cef
Tamas Jozsa <tamas.jozsa@eng.ox.ac.uk>
2022-02-22 18:33:58 +0000

    res2image conversion added to test script

--- perfusion_runner.sh
+++ perfusion_runner.sh
@@ -17,7 +17,11 @@ fi
 
 python3 BC_creator.py
 mpirun -n 6 python3 basic_flow_solver.py
+python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_healthy/
 mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_LMCAo.yaml
+python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_LMCAo/
 mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_RMCAo.yaml
+python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_RMCAo/
+
 mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_LMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_LMCAo/perfusion.xdmf
 mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_RMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_RMCAo/perfusion.xdmf

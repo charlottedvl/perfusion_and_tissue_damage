ce4d3cfa33c2c52b08d2cc81a0baa907a2838853
Tamas Jozsa <tamas.jozsa@eng.ox.ac.uk>
2022-03-16 10:44:41 +0000

    image conversion updated

--- perfusion_runner.sh
+++ perfusion_runner.sh
@@ -12,16 +12,16 @@ then
     echo "The permeability tensor form has been computed already"
 else
     echo "The permeability tensor form will be computed" 
-    mpirun -n 6 python3 permeability_initialiser.py
+    mpirun -n 6 python3 permeability_initialiser.py --config_file ./config_examples/config_permeability_initialiser4testing.yaml
 fi
 
 python3 BC_creator.py
 mpirun -n 6 python3 basic_flow_solver.py
-python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_healthy/
+python3 convert_res2img.py --config_file ../VP_results/p0000/perfusion_healthy/settings.yaml
 mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_LMCAo.yaml
-python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_LMCAo/
+python3 convert_res2img.py --config_file ../VP_results/p0000/perfusion_LMCAo/settings.yaml
 mpirun -n 6 python3 basic_flow_solver.py --config_file ./config_basic_flow_solver_RMCAo.yaml
-python3 convert_res2img.py --res_fldr ../VP_results/p0000/perfusion_RMCAo/
+python3 convert_res2img.py --config_file ../VP_results/p0000/perfusion_RMCAo/settings.yaml
 
 mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_LMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_LMCAo/perfusion.xdmf
 mpirun -n 6 python3 infarct_calculation_thresholds.py --config_file ./config_basic_flow_solver_RMCAo.yaml --baseline ../VP_results/p0000/perfusion_healthy/perfusion.xdmf --occluded ../VP_results/p0000/perfusion_RMCAo/perfusion.xdmf

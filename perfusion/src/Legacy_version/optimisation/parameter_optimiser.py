e5f76bebbd301ad4c29e38dcfa3581278b99af8e
Charlotte DEVILLE <dcharlotte92@gmail.com>
2025-07-24 14:47:08 +0100

    refactor(plot): create an optimisation folder and move relevant files inside

--- perfusion/src/Legacy_version/optimisation/parameter_optimiser.py
+++ perfusion/src/Legacy_version/optimisation/parameter_optimiser.py
@@ -137,9 +137,9 @@ numpy.set_printoptions(linewidth=200)
 parameters['ghost_mode'] = 'none'
 
 # added module
-from src.Legacy_version.io import IO_fcts, basic_flow_solver_IO
-from src.Legacy_version.utils import suppl_fcts
-from src.Legacy_version.utils import finite_element_fcts as fe_mod
+from ..io import IO_fcts, basic_flow_solver_IO
+from ..utils import suppl_fcts
+from ..utils import finite_element_fcts as fe_mod
 
 # solver runs is "silent" mode
 set_log_level(50)
